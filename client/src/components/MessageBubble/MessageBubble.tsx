e306e1acf1a39205e4b7a02ef852301153833ea0
OlegWock <olegwock@gmail.com>
2024-04-27 19:26:49 +0200

    Improvements to message actions

--- client/src/components/MessageBubble/MessageBubble.tsx
+++ client/src/components/MessageBubble/MessageBubble.tsx
@@ -6,7 +6,8 @@ import clsx from 'clsx';
 import { CodeBlock } from '@client/components/MessageBubble/CodeBlock';
 import { Button } from '@client/components/Button';
 import { HiArrowPath, HiChevronLeft, HiChevronRight } from 'react-icons/hi2';
-import { PiArrowsSplit } from "react-icons/pi";
+import { PiArrowsSplit, PiCopy, PiCopyLight } from "react-icons/pi";
+import { createStrictContext } from '@client/utils/context';
 
 
 export type MessageBubbleActionsProp = {
@@ -19,28 +20,53 @@ export type MessageBubbleActionsProp = {
   onDuplicate?: VoidFunction;
 };
 
-const MessageBubbleActions = ({ variants, onRegenerate, onDuplicate }: MessageBubbleActionsProp) => {
-  return (<div className={styles.actionsWrapper}>
-    {!!variants && <div className={styles.variants}>
-      <Button variant="borderless" onClick={() => variants.onSwitchVariant(false)}><HiChevronLeft /></Button>
-      <span>{variants.current}/{variants.total}</span>
-      <Button variant="borderless" onClick={() => variants.onSwitchVariant(true)}><HiChevronRight /></Button>
-    </div>}
-    <div className={styles.spacer} />
-    <div className={styles.actions}>
-      {!!onRegenerate && <Button onClick={onRegenerate} variant="borderless"><HiArrowPath /></Button>}
-      {!!onDuplicate && <Button onClick={onDuplicate} variant="borderless"><PiArrowsSplit /></Button>}
-    </div>
-  </div>)
-}
-
 export type MessageBubbleProps = {
   message: MessageSchemaType;
   senderName: string;
   actions?: MessageBubbleActionsProp;
 }
 
-export const MessageBubble = ({ message, senderName, actions }: MessageBubbleProps) => {
+const [Provider, useBubbleContext] = createStrictContext<MessageBubbleProps>('MessageBubbleContext');
+
+const MessageBubbleActions = () => {
+  const onCopy = () => {
+    // TODO: show feedback on copy
+    // TODO: consider copying rich text along with plaintext (so when pasted in Google Docs for example, it will preserve formatting)
+    navigator.clipboard.writeText(message.text);
+  };
+
+  const { actions = {}, message } = useBubbleContext();
+  const { variants, onRegenerate, onDuplicate } = actions;
+  return (
+    <div className={styles.actionsWrapper}>
+      {!!variants && <div className={styles.variants}>
+        <Button
+          variant="borderless"
+          disabled={variants.current === 1}
+          onClick={() => variants.onSwitchVariant(false)}
+        >
+          <HiChevronLeft />
+        </Button>
+        <span>{variants.current}/{variants.total}</span>
+        <Button
+          variant="borderless"
+          disabled={variants.current === variants.total}
+          onClick={() => variants.onSwitchVariant(true)}
+        >
+          <HiChevronRight />
+        </Button>
+      </div>}
+      <div className={styles.spacer} />
+      <div className={styles.actions}>
+        {!!onRegenerate && <Button onClick={onRegenerate} variant="borderless"><HiArrowPath /></Button>}
+        {!!onDuplicate && <Button onClick={onDuplicate} variant="borderless"><PiArrowsSplit /></Button>}
+        <Button onClick={onCopy} variant="borderless"><PiCopyLight /></Button>
+      </div>
+    </div>)
+}
+
+export const MessageBubble = (props: MessageBubbleProps) => {
+  const { message, senderName, actions } = props;
   const showPlaceholder = message.isGenerating && !message.text;
 
   // TODO: what actions we need to support:
@@ -49,25 +75,30 @@ export const MessageBubble = ({ message, senderName, actions }: MessageBubblePro
   // * Editing message with regeneration (only user)
   // * Duplicating response (ai only)
 
+  // TODO: copy message button
+
   // TODO: markdown-to-jsx doesn't have option to preserve line breaks, need to either hack something or replace it with another library
 
-  return (<div className={clsx(styles.MessageBubble, styles[message.sender])}>
-    <div className={styles.senderName}>{senderName}</div>
-    {showPlaceholder
-      ? (<SyncLoader loading className={styles.loader} color='var(--gray-4)' size={'0.5rem'} />)
-      : <>
-        <Markdown
-          className={styles.content}
-          options={{
-            overrides: {
-              code: CodeBlock,
-            }
-          }}
-        >
-          {message.text}
-        </Markdown>
-        {!!actions && <MessageBubbleActions {...actions} />}
-      </>}
-    {/* TODO: show datetime of the message */}
-  </div>)
+  return (
+    <Provider value={{ ...props }}>
+      <div className={clsx(styles.MessageBubble, styles[message.sender])}>
+        <div className={styles.senderName}>{senderName}</div>
+        {showPlaceholder
+          ? (<SyncLoader loading className={styles.loader} color='var(--gray-4)' size={'0.5rem'} />)
+          : <>
+            <Markdown
+              className={styles.content}
+              options={{
+                overrides: {
+                  code: CodeBlock,
+                }
+              }}
+            >
+              {message.text}
+            </Markdown>
+            <MessageBubbleActions />
+          </>}
+        {/* TODO: show datetime of the message */}
+      </div>
+    </Provider>)
 };
