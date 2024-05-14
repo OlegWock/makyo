bf4ec092f02e2f9b37352b0756c9f2a96782ca23
OlegWock <olegwock@gmail.com>
2024-05-14 20:53:19 +0200

    Updates to deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,9 @@ set -e
 echo "Nuking database"
 bun run drizzle:nuke
 
-echo "Migrating database"
-bun run drizzle:migrate
+# Migrate is part of nuke
+# echo "Migrating database"
+# bun run drizzle:migrate
 
 echo "Starting server"
 bun run server/index.ts
