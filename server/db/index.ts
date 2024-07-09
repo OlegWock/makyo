3a483412c3c680fa6a05481e7e8a1019530899d6
OlegWock <olegwock@gmail.com>
2024-07-09 09:45:07 +0200

    Better handle data folder

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,5 +1,6 @@
 import { drizzle } from "drizzle-orm/bun-sqlite";
 import { Database } from "bun:sqlite";
+import { existsSync, mkdirSync } from "node:fs";
 import { resolve } from "node:path"
 import * as schema from './schema';
 
@@ -8,10 +9,14 @@ if (!process.env.MAKYO_DATA_FOLDER) {
   process.exit(1);
 }
 
+if (!existsSync(resolve(process.env.MAKYO_DATA_FOLDER))) {
+  mkdirSync(resolve(process.env.MAKYO_DATA_FOLDER), { recursive: true });
+}
+
 if (process.env.MAKYO_MACOS_HOMEBREW_SQLITE_PATH) {
   Database.setCustomSQLite(process.env.MAKYO_MACOS_HOMEBREW_SQLITE_PATH);
 }
-const sqlite = new Database(resolve(process.cwd(), `${process.env.MAKYO_DATA_FOLDER}/makyo.db`));
+const sqlite = new Database(resolve(process.env.MAKYO_DATA_FOLDER, `makyo.db`));
 sqlite.loadExtension(resolve(process.cwd(), `server/db/sqlite/${process.platform}-${process.arch}/define`));
 sqlite.loadExtension(resolve(process.cwd(), `server/db/sqlite/${process.platform}-${process.arch}/fuzzy`));
 sqlite.loadExtension(resolve(process.cwd(), `server/db/sqlite/${process.platform}-${process.arch}/regexp`));
